8912c7107d5dd68094f6a74a6dce0227c44f2ea2
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-10-11 21:33:53 +0300

    TestNG

---
+++ src/test/java/Tests/day07/C01_Priority.java
@@ -0,0 +1,60 @@
+package Tests.day07;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class C01_Priority {
+
+    WebDriver driver;
+
+/*
+     3 test method'u olusturun.
+     1- Amazon ana sayfasina,
+     2- Ebay ana sayfasina,
+     3- Facebook ana sayfasina gitsin.
+     Ve sayfa title'larini yazdirin.
+ */
+
+    @BeforeMethod
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        driver.close();
+    }
+
+    @Test (priority = 2)
+    public void amazonTest() {
+
+        driver.get("https://www.amazon.com/");
+        System.out.println(driver.getTitle());
+    }
+
+    @Test (priority = 3)
+    public void ebayTest() {
+
+        driver.get("https://www.ebay.com/");
+        System.out.println(driver.getTitle());
+    }
+
+    @Test (priority = 1)
+    public void facebookTest() {
+
+        driver.get("https://www.facebook.com/");
+        System.out.println(driver.getTitle());
+    }
+}
