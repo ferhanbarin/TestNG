88007a0600c39d8ca887330cc496778b92afccd2
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-10-15 23:18:25 +0300

    TestNG

---
+++ src/test/java/Tests/day11/C05_UploadFile.java
@@ -0,0 +1,34 @@
+package Tests.day11;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C05_UploadFile extends TestBase {
+
+/*
+    1.Tests packagenin altina bir class oluşturun : C05_UploadFile
+    2.https://the-internet.herokuapp.com/upload adresine gidelim
+    3.chooseFile butonuna basalim
+    4.Yuklemek istediginiz dosyayi secelim.
+    5.Upload butonuna basalim.
+    6.“File Uploaded!” textinin goruntulendigini test edelim.
+*/
+
+    @Test
+    public void test() {
+
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+        String dosyaYolu = System.getProperty("user.home") + "\\Downloads\\Profile.jpeg";
+
+        WebElement dosyaYükle = driver.findElement(By.id("file-upload"));
+        dosyaYükle.sendKeys(dosyaYolu);
+
+        driver.findElement(By.id("file-submit")).click();
+
+        Assert.assertTrue(driver.findElement(By.tagName("h3")).isDisplayed());
+    }
+}
