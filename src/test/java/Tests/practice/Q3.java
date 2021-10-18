dfc915832c5a6632fd3dcd83825b8c0fdf99a16f
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-10-18 17:08:19 +0300

    TestNG

---
+++ src/test/java/Tests/practice/Q3.java
@@ -0,0 +1,13 @@
+package Tests.practice;
+
+public class Q3 {
+
+/*
+     Go to http://demo.guru99.com/test/drag_drop.html URL.
+     Drag and drop the BANK button to the Account section in DEBIT SIDE.
+     Drag and drop the SALES button to the Account section in CREDIT SIDE.
+     Drag and drop the 5000 button to the Amount section in DEBIT SIDE.
+     Drag and drop the second 5000 button to the Amount section in CREDIT SIDE.
+*/
+
+}
