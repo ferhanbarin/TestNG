712b53f31106bc4bed4ca15a9d28aa1d23c29378
Ferhan BARIN <ferhanbarin@yahoo.com>
2021-12-24 02:35:02 +0200

    TestNG

--- src/test/java/Tests/day07/C04_DropDownAmazon.java
+++ src/test/java/Tests/day07/C04_DropDownAmazon.java
@@ -11,7 +11,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
